fe03d97e87e423b9f8c414773b8475b6c71866b2
tony9020 <tonychristopherofficial@gmail.com>
2024-03-11 02:56:00 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+// app/layout.tsx
+import type { Metadata } from "next"
+import { Libre_Franklin, Fjalla_One } from "next/font/google"
+import "./globals.css"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+import Banner from "@/components/Banner"
+import { ThemeProvider } from "@/components/theme-provider"
+const sans = Libre_Franklin({ subsets: ["latin"], variable: "--font-sans" })
+const display = Fjalla_One({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-display",
+})
+
+export const metadata: Metadata = {
+  title: "ProLine Content Corps",
+  description: "Build and scale faster with ProLine and Cosmic Blocks",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${display.variable} ${sans.variable} font-sans md:p-0 bg-white dark:bg-black h-dvh w-full`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div>
+            <Banner />
+            <Header />
+            {children}
+          </div>
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,38 @@
+// components/header.tsx
+import Link from "next/link"
+import { cosmic } from "@/cosmic/client"
+import { NavMenu } from "@/cosmic/blocks/navigation-menu/NavMenu"
+
+export default async function Header() {
+  // Header data
+  const { object: settings } = await cosmic.objects
+    .findOne({
+      type: "global-settings",
+      slug: "settings",
+    })
+    .props("metadata")
+    .depth(1)
+
+  return (
+    <nav className="space-x-4 sticky top-0 bg-white/20 dark:bg-black/20 backdrop-blur-lg py-2 w-full z-[9999]">
+      <div className="flex w-full items-center md:container justify-between pl-2 pr-4">
+        <Link href="/">
+          <img
+            src={`${settings.metadata.logo.imgix_url}?w=500&auto=format,compression`}
+            alt={settings.metadata.company}
+            className="h-10 m-auto dark:hidden"
+          />
+          <img
+            src={`${settings.metadata.dark_logo.imgix_url}?w=500&auto=format,compression`}
+            alt={settings.metadata.company}
+            className="h-10 m-auto hidden dark:block"
+          />
+        </Link>
+        <NavMenu
+          query={{ type: "navigation-menus", slug: "header" }}
+          hasMobileMenu
+        />
+      </div>
+    </nav>
+  )
+}
