8055e66958fa4c32cdab2bb3f63b3359539f1417
Tony Christopher <73989159+tony9020@users.noreply.github.com>
2024-03-11 09:02:39 +0530

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,10 +6,10 @@ export default function Banner(): JSX.Element {
       <a href="https://github.com/cosmicjs/agency-template" target="_parent">
         <div className="flex items-center space-x-1 bg-zinc-100 px-4 py-2 text-xs dark:bg-zinc-800 md:text-sm lg:text-base rounded-full">
           <span className="text-zinc-800 dark:text-zinc-200">
-            Source code available on
+            24 * 7 available on
           </span>
           <span className="text-green-700 font-medium dark:text-green-300">
-            GitHub
+            WhatsApp
           </span>
           <ChevronRight className="w-4 h-4" />
         </div>
