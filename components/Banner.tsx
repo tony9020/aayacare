a346d8eea43eeb4c37c82ec51ec2d5b8c83ffbc0
tony9020 <tonychristopherofficial@gmail.com>
2024-03-11 16:37:55 +0530

    banner fixes and logo addition
    Signed-off-by: tony9020 <tonychristopherofficial@gmail.com>

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,4 +1,4 @@
-import { ChevronRight } from "lucide-react"
+import { ChevronRight, MessageCircle } from "lucide-react"
 
 export default function Banner(): JSX.Element {
   return (
@@ -9,8 +9,9 @@ export default function Banner(): JSX.Element {
           target="_parent"
         >
           <div className="glow flex items-center space-x-1 bg-zinc-100 px-4 py-2 text-xs dark:bg-zinc-800 md:text-sm lg:text-base rounded-full">
+            <MessageCircle color="#0cc144" />
             <span className="text-zinc-800 dark:text-zinc-200">
-              24 * 7 available on
+              24 * 7 Online on
             </span>
             <span className="text-green-700 font-medium dark:text-green-300">
               WhatsApp
@@ -26,9 +27,9 @@ export default function Banner(): JSX.Element {
         >
           <div className="glow flex items-center space-x-1 bg-zinc-100 px-4 py-2 text-xs dark:bg-zinc-800 md:text-sm lg:text-base rounded-full">
             <span className="text-green-700 font-medium dark:text-green-300">
+              <MessageCircle color="#0cc144" />
               chat
             </span>
-            <ChevronRight className="w-4 h-4" />
           </div>
         </a>
       </div>
